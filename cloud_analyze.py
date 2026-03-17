387a89ca08929339c984a49f9d20efb87caf6ca1
Samvardhan Singh <104402477+samvardhan03@users.noreply.github.com>
2026-03-17 19:55:16 +0530

    Delete cloud_analyze.py

--- cloud_analyze.py
+++
@@ -1,8 +0,0 @@
-from google.cloud import language_v1
-
-client = language_v1.LanguageServiceClient()
-
-# Analyze text
-document = language_v1.Document(content=user_query, type_=language_v1.Document.Type.PLAIN_TEXT)
-response = client.analyze_sentiment(request={'document': document})
-sentiment = response.document_sentiment.score
